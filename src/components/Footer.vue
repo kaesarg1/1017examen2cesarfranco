87f6061ae17e5f38f6a16eb138fe497b069dfd01
kaesarg1 <franco.cesar@gmail.com>
2021-02-12 21:49:35 -0500

    mes premiers composants

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="footer">
+        <h1>Footer</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <h1>Header</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+
+
+}
+</script>
+
+<style>
+
+</style>
